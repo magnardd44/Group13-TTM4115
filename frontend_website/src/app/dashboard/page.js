0514a05d700fc4d259cf8eb9edeb3034c6281fb0
Magnar <magnard44@gmail.com>
2024-04-15 16:09:43 +0200

    added google login and dashboard

--- frontend_website/src/app/dashboard/page.js
+++ frontend_website/src/app/dashboard/page.js
@@ -79,7 +79,7 @@ const CurrentlyCharging = ({presentage}) => {
   return (
     <>
     {/* <p className="text-xl">Charging...</p> */}
-    <div className="battery mt-8">
+    <div className="battery">
       <div className="flex justify-center items-center flex-col h-full">
         <p className="text-xl relative font-semibold">Charging...</p>
         <p className="relative text-xl">{presentage}%</p>
