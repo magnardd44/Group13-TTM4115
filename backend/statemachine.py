bb4256feb94db941d02339775f135cf851596c31
Jonatan <jkaalen@stud.ntnu.no>
2024-04-23 15:00:19 +0200

    MQTT and state machine

--- backend/statemachine.py
+++ backend/statemachine.py
@@ -1,32 +1,76 @@
-from stmpy import Machine, Driver
+from stmpy import Machine, Driver,  get_graphviz_dot
+
+from threading import Thread
+
+import paho.mqtt.client as mqtt
+
+topic = "/group-13/charger_server"
+broker, port = "test.mosquitto.org", 1883
+message = ""
+
+class MQTT_Client_1:
+    def __init__(self):
+        self.count = 0
+        self.client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION1)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("on_connect(): {}".format(mqtt.connack_string(rc)))
+
+    def on_message(self, client, userdata, msg):
+        message_str = msg.payload.decode('utf-8').strip()
+        if message_str.startswith("charger:"):
+            print(message_str[8:])
+            message = message_str[8:]
+            self.stm_driver.send(message_str[8:], "server")
+
+    def start(self, broker, port):
+
+        print("Connecting to {}:{}".format(broker, port))
+        self.client.connect(broker, port)
+
+        self.client.subscribe(topic)
+
+        try:
+            # line below should not have the () after the function!
+            thread = Thread(target=self.client.loop_forever)
+            thread.start()
+        except KeyboardInterrupt:
+            print("Interrupted")
+            self.client.disconnect()
 
 class Server:
             
     def __init__(self):
-        pass
-    
-    def what_now(self):
-        send = input("What do you want to send: ")
-        self.stm.send(send)
-    def mark_charger_free():
+        self.payment_info = ""
+        
+    def mark_charger_free(self):
         print("Charger is free")
     
-    def mark_charger_occupied():
+    def mark_charger_occupied(self):
         print("Charger is occupied")
     
-    def start_validation():
+    def start_validation(self):
         print("Validation started")
     
-    def start_charge():
+    def send_ok(self):
+        self.mqtt_client.publish(topic, "server:server_ok")
         print("Charging started")
         
-    def store_info():
+    def store_info(self):
         print("Info stored")
 
-    def issue_payment():
-        print("payment issued")
+    def issue_payment(self):
+        print("Payment issued")
         
-    def notify_charger():
+    def reject_information(self):
+        if message == "tag_received":
+            self.mqtt_client.publish(topic, "server:tag_info_rejected")
+        elif message == "licence_received":
+            self.mqtt_client.publish(topic, "server:licence_info_rejected")
+        else:
+            self.mqtt_client.publish(topic, "server:app_failed")
         print("Charger notified")
 
 
@@ -34,62 +78,87 @@ server = Server()
         
 # initial transition
 t0 = {'source':'initial',
-      'effect':'what_now',
       'target':'idle'}
 
-t1 = {'trigger':'charger_connected',
+# idle transitions
+t_idle = {'trigger':'charger_connected',
       'source':'idle',
-      'effect':'mark_charger_occupied; what_now',
+      'effect':'mark_charger_occupied',
       'target':'connected'}
 
-t2 
-
-t3 = {'trigger':'tag_received', 
+# connected transitions
+t_conn_1 = {'trigger':'tag_received', 
       'source':'connected', 
-      'effect':'start_validation; what_now',
-      'target':'validate'}
+      'effect':'start_validation',
+      'target':'validate_pay_info'}
 
-t3 = {'trigger':'licence_received', 
+t_conn_2 = {'trigger':'licence_received', 
       'source':'connected', 
-      'effect':'start_validation; what_now',
-      'target':'validate'}
+      'effect':'start_validation',
+      'target':'validate_pay_info'}
 
-t4 = {'trigger':'app_connected', 
+t_conn_3 = {'trigger':'app_connected', 
       'source':'connected', 
-      'effect':'start_validation; what_now',
-      'target':'validate'}
+      'effect':'start_validation',
+      'target':'validate_pay_info'}
 
-t5 = {'trigger':'all_denied', 
-      'source':'validate', 
-      'effect':'notify_charger; what_now',
+t_conn_4 = {'trigger':'identification_failed', 
+      'source':'connected',
       'target':'wait_disconnect'}
 
-t6 = {'trigger':'accepted', 
-      'source':'validate', 
-      'effect':'start_charger; store_info; what_now',
+t_conn_5 = {'trigger':'charger_disconnected',
+      'source':'connected',
+      'effect':'mark_charger_free',
+      'target':'idle'}
+
+# validate_pay_info transitions
+t_val_1 = {'trigger':'accepted', 
+      'source':'validate_pay_info', 
+      'effect':'send_ok; store_info',
       'target':'charging'}
 
-t7 = {'trigger':'charge_complete',
+t_val_2 = {'trigger':'invalid_information', 
+      'source':'validate_pay_info', 
+      'effect':'reject_information',
+      'target':'connected'}
+
+t_val_3 = {'trigger':'charger_disconnected',
+      'source':'validate_pay_info',
+      'effect':'mark_charger_free',
+      'target':'idle'}
+
+# Charging transitions
+t_char_1 = {'trigger':'charge_complete',
       'source':'charging',
-      'effect':'issue_payment; what_now',
+      'effect':'issue_payment',
       'target':'wait_disconnect'}
 
-t8 = {'trigger':'charger_disconnected',
+t_char_2 = {'trigger':'charger_disconnected',
       'source':'charging',
-      'effect':'issue_payment; mark_charger_free; what_now',
+      'effect':'issue_payment; mark_charger_free',
       'target':'idle'}
 
-t9 = {'trigger':'charger_disconnected',
+# Wait transitions
+t_wait = {'trigger':'charger_disconnected',
       'source':'wait_disconnect',
-      'effect':'mark_charger_free; what_now',
+      'effect':'mark_charger_free',
       'target':'idle'}
+
 
 
 
 # Change 4: We pass the set of states to the state machine
-machine = Machine(name='server', transitions=[t0, t1, t2, t3, t4 ,t5, t6, t7, t8, t9, t10, t11, t12], obj=charger)
+machine = Machine(name='server', transitions=[t0, t_idle, t_char_1, t_char_2, t_conn_1 ,t_conn_2, t_conn_3, t_conn_4, t_conn_5, t_val_1, t_val_2, t_val_3, t_wait], obj=server)
+with open("graph_backend.gv", "w") as file:
+      print(get_graphviz_dot(machine), file=file)
 server.stm = machine
 
 driver = Driver()
-driver.add_machine(machine)
+driver.add_machine(machine) 
+
+myclient = MQTT_Client_1()
+server.mqtt_client = myclient.client
+myclient.stm_driver = driver
+
 driver.start()
+myclient.start(broker, port)
--- charger/statemachine.py
+++ charger/statemachine.py
@@ -1,31 +1,105 @@
-from stmpy import Machine, Driver
+from stmpy import Machine, Driver, get_graphviz_dot
 
-class Charger:
-            
+import time
+
+from threading import Thread
+
+import paho.mqtt.client as mqtt
+
+topic = "/group-13/charger_server"
+broker, port = "test.mosquitto.org", 1883
+
+class MQTT_Client_1:
+    def __init__(self):
+        self.count = 0
+        self.client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION1)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("on_connect(): {}".format(mqtt.connack_string(rc)))
+
+    def on_message(self, client, userdata, msg):
+        message_str = msg.payload.decode('utf-8').strip()
+        if message_str.startswith("server:"):
+            print(message_str[7:])
+            self.stm_driver.send(message_str[7:], "charger")
+
+
+    def start(self, broker, port):
+
+        print("Connecting to {}:{}".format(broker, port))
+        self.client.connect(broker, port)
+
+        self.client.subscribe(topic)
+
+        try:
+            # line below should not have the () after the function!
+            thread = Thread(target=self.client.loop_forever)
+            thread.start()
+        except KeyboardInterrupt:
+            print("Interrupted")
+            self.client.disconnect()
+
+class Charger:    
     def __init__(self):
         pass
-        
 
-    def what_now(self):
-        send = input("What do you want to send: ")
-        self.stm.send(send)
-    def check_tag(help):
-        print('blablabala') 
-        
-    def connect_to_server(help):
+    #Server functions
+    def connect_to_server(self):
+        self.mqtt_client.publish(topic, "charger:charger_connected")
         print('Connected to server')
-        
+
+    def disconnect_from_server(self):
+        self.mqtt_client.publish(topic, "charger:charger_disconnected")
+        print('Disconnecting from server')
+
+    def send_complete(self):
+        self.mqtt_client.publish(topic, "charger:charge_complete")
+        print("Sending complete to server")
+    
+    #Check functions
     def check_camera(self):
+        self.mqtt_client.publish(topic, "charger:licence_recieved")
         print('Checking camera')
         
+    def check_tag(self):
+        self.mqtt_client.publish(topic, "charger:tag_received")
+        print('blablabala') 
+      
+    def check_app(self):
+        self.mqtt_client.publish(topic, "charger:app_connected") # remove this when the app is working
+        print('Checking app')
+    
+    
+    #Charge functions
     def start_charge(self):
         print('Starting charge')
-        
+        for i in range(1,101):
+            if not self.mqtt_client.plugged:
+                break
+            time.sleep(0.5)
+            print(str(i)+"%")
+
+    def stop_charge(self):
+        print('Stopping charge')
+    
+    #Display functions
     def display_charge(self):
         print('Displaying charge')
         
-    def check_app(self):
-        print('Checking app')
+    def display_tag_check(self):
+        self.mqtt_client.publish(topic, "server:tag_detected")
+        print("Checking tag, if nothing happens, press button")
+        
+    def display_camera_check(self):
+        print("Checking camera, if nothing happens, press button")
+        
+    def display_connect_with_app(self):
+        print("Please connect through app")
+
+    def display_waiting_for_payment(self):
+        print("Checking info")
         
     def display_complete(self):
         print('Display completed')
@@ -34,88 +108,132 @@ class Charger:
         print('Stopping display')
         
     def display_error_message(self):
+        self.mqtt_client.publish(topic, "charger:identfication_failed")
         print('Error')
-        
-    def disconnect_from_server(self):
-        print('Disconnecting from server')
-        
-    def issue_payment(self):
-        print('Issuing payment')
-        
 
+        
 charger = Charger()
         
 # initial transition
-t0 = {'source':'initial',
-      'effect':'what_now',
+t0 = {'source':'initial', 
       'target':'idle'}
 
-t1 = {'trigger':'car_plugged_in',
+#transitions from idle
+t_idle_1 = {'trigger':'car_plugged_in',
       'source':'idle',
-      'effect':'check_tag; what_now',
+      'effect':'connect_to_server; check_tag; display_tag_check',
       'target':'tag_identify'}
 
-t2 = {'trigger':'tag_detected', 
+#transitions from tag_identify
+t_tag_1 = {'trigger':'tag_detected', 
       'source':'tag_identify', 
-      'effect':'start_charge; display_charge; what_now',
-      'target':'charging'}
+      'effect':'display_waiting_for_payment',
+      'target':'server_validate'}
 
-t3 = {'trigger':'tag_failed', 
+t_tag_2 = {'trigger':'button', 
       'source':'tag_identify', 
-      'effect':'check_camera; what_now',
+      'effect':'check_camera; display_camera_check',
       'target':'camera_identify'}
 
-t4 = {'trigger':'licence_detected', 
-      'source':'camera_identify', 
-      'effect':'start_charge; display_charge; what_now',
-      'target':'charging'}
-
-t5 = {'trigger':'camera_failed', 
-      'source':'camera_identify', 
-      'effect':'check_app; start_timer("t", 5000); what_now',
-      'target':'app_identify'}
-
-t6 = {'trigger':'app_start', 
-      'source':'app_identify', 
-      'effect':'start_charge; display_charge; stop_timer("t"); what_now',
-      'target':'charging'}
-
-t7 = {'trigger':'app_failed',
-      'source':'app_identify',
-      'effect':'display_error_message; stop_timer("t"); what_now',
-      'target':'identification_failed'}
-
-t8 = {'trigger':'t',
-      'source':'app_identify',
-      'effect':'display_error_message; what_now',
-      'target':'identification_failed'}
-
-t9 = {'trigger':'car_unplugged',
-      'source':'identification_failed',
-      'effect':'disconnect_from_server; what_now',
+t_tag_3 = {'trigger':'car_unplugged', 
+      'source':'tag_identify', 
+      'effect':'disconnect_from_server; stop_display',
       'target':'idle'}
 
-t10 = {'trigger':'charge_complete',
+#transitions from camera_identify
+t_camera_1 = {'trigger':'licence_detected', 
+      'source':'camera_identify', 
+      'effect':'display_waiting_for_payment',
+      'target':'server_validate'}
+
+t_camera_2 = {'trigger':'button', 
+      'source':'camera_identify', 
+      'effect':'display_connect_with_app; start_timer("app_timer", 5000)',
+      'target':'app_identify'}
+
+t_camera_3 = {'trigger':'car_unplugged', 
+      'source':'camera_identify', 
+      'effect':'disconnect_from_server; stop_display',
+      'target':'idle'}
+
+#transitions from server_validate
+t_server_1 = {'trigger':'tag_info_rejected', 
+      'source':'server_validate', 
+      'effect':'check_camera; display_camera_check',
+      'target':'camera_identify'}
+
+t_server_2 = {'trigger':'server_ok', 
+      'source':'server_validate', 
+      'effect':'start_charge; display_charge',
+      'target':'charging'}
+
+t_server_3 = {'trigger':'licence_info_rejected', 
+      'source':'server_validate', 
+      'effect':'display_connect_with_app; start_timer("app_timer", 5000)',
+      'target':'app_identify'}
+
+t_server_4 = {'trigger':'car_unplugged', 
+      'source':'server_validate', 
+      'effect':'disconnect_from_server; stop_display', 
+      'target':'idle'}
+#transitions from app_identify
+t_app_1 = {'trigger':'app_start', 
+      'source':'app_identify', 
+      'effect':'start_charge; display_charge',
+      'target':'charging'}
+
+t_app_2= {'trigger':'app_failed',
+      'source':'app_identify',
+      'effect':'display_error_message',
+      'target':'identification_failed'}
+
+t_app_3 = {'trigger':'app_timer',
+      'source':'app_identify',
+      'effect':'display_error_message',
+      'target':'identification_failed'}
+
+t_app_4 = {'trigger':'car_unplugged',
+      'source':'app_identify',
+      'effect':'disconnect_from_server; stop_display',
+      'target':'idle'}
+
+#transitions from identification
+t_identification = {'trigger':'car_unplugged',
+      'source':'identification_failed',
+      'effect':'disconnect_from_server',
+      'target':'idle'}
+
+#transitions from charging
+t_charging_1 = {'trigger':'charge_complete',
       'source':'charging',
-      'effect':'display_complete; issue_payment; what_now',
+      'effect':'display_complete; send_complete',
       'target':'charge_complete'}
 
-t11 = {'trigger':'car_unplugged',
+t_charging_2 = {'trigger':'car_unplugged',
        'source':'charging',
-       'effect':'stop_display; disconnect_from_server; issue_payment; what_now',
+       'effect':'stop_display; disconnect_from_server',
        'target':'idle'}
 
-t12 = {'trigger':'car_unplugged',
+#transitions from charge_complete
+t_complete = {'trigger':'car_unplugged',
       'source':'charge_complete',
-      'effect':'disconnect_from_server; stop_display; what_now',
+      'effect':'disconnect_from_server; stop_display',
       'target':'idle'}
 
 
 
 # Change 4: We pass the set of states to the state machine
-machine = Machine(name='charger', transitions=[t0, t1, t2, t3, t4 ,t5, t6, t7, t8, t9, t10, t11, t12], obj=charger)
+machine = Machine(name='charger', transitions=[t0, t_idle_1, t_tag_1, t_tag_2, t_tag_3, t_camera_1, t_camera_2, t_camera_3, t_server_1, t_server_2, t_server_3, t_server_4, t_app_1, t_app_2, t_app_3, t_app_4, t_identification, t_charging_1, t_charging_2, t_complete], obj=charger)
+with open("graph.gv", "w") as file:
+      print(get_graphviz_dot(machine), file=file)
 charger.stm = machine
 
 driver = Driver()
-driver.add_machine(machine)
+driver.add_machine(machine) 
+
+myclient = MQTT_Client_1()
+charger.mqtt_client = myclient.client
+myclient.stm_driver = driver
+
 driver.start()
+myclient.start(broker, port)
