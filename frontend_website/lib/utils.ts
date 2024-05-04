e19603b1aa4edcca45e93eadbf168090b0bc7f70
Magnar <magnard44@gmail.com>
2024-05-04 21:49:05 +0200

    .

--- frontend_website/lib/utils.ts
+++ frontend_website/lib/utils.ts
@@ -10,7 +10,7 @@ export function cn(...inputs) {
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_ANON
   );
 }
 
