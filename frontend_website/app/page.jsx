90a5e09a390014803bf215bb81fcb47ceb7c3210
Magnar <magnard44@gmail.com>
2024-05-06 00:22:26 +0200

    removed force redirect

--- frontend_website/app/page.jsx
+++ frontend_website/app/page.jsx
@@ -1,23 +1,7 @@
 "use client";
-import { redirect, useRouter, usePathname } from "next/navigation";
 import Login from "../components/Login/Login";
-import { getUser } from "./utils";
-import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter();
-  const pathName = usePathname();
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      let user = await getUser();
-
-      if (user && pathName == "/") router.push("/dashboard");
-    };
-
-    fetchUser();
-  }, []);
-
   return (
     <main>
       <Login />
